a031908c314156718f92725c03b80b970b29ede8
Alison Williams <apw5011@gmail.com>
2020-11-23 23:54:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,12 +4,16 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0
+  @@genres = []
+  @@artists = []
   
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres << genre
+    @@artists << artist
   end 
   
   def self.count
@@ -17,12 +21,10 @@ class Song
   end 
   
   def self.genres
-    @@genres = []
     @@genres.uniq!
   end 
   
-  def self.artists 
-    @@artists = []
+  def self.artists
     @@aritsts.uniq!
   end 
   
